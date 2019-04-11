da55de1d1fe0672a6509f23e10db4b42b3602431
MengZhiXingHun <2902418687@qq.com>
2019-04-11 08:55:15 +0800

    wahahaha

---
+++ GaoDe/app/src/main/java/com/dream/gaode/MainActivity.java
@@ -0,0 +1,23 @@
+package com.dream.gaode;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.amap.api.maps.MapView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private MapView mMapView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        //获取地图控件引用
+        mMapView = (MapView) findViewById(R.id.map);
+        //在activity执行onCreate时执行mMapView.onCreate(savedInstanceState)，创建地图
+        mMapView.onCreate(savedInstanceState);
+
+    }
+}
